efe7e0917948964cb450d49c41ada366c1206c18
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-09-23 05:50:15 -0400

    commit tree 8ad762b11c369ae169f3b97023e9f8eadfc70802

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.map do |name|
+    "Hello, my name is #{name}."
+  end 
+end
+
+def assign_rooms(attendees)
+  attendees.map.with_index(1) do |attendee, room_num|
+    "Hello, #{attendee}! You'll be assigned to room #{room_num}!"
+  end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |number|
+    puts number
+end
+end
